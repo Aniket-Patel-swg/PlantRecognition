797ad01d751dde3641f74bec4eac5ca578656ec0
Aniket-Patel-swg <patelaniket1207@gmail.com>
2023-11-25 10:08:59 +0530

    home page changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
 
@@ -9,6 +10,26 @@ export default function Home() {
       <Link href="/plantinfo">Get Info Now</Link>
 
       <Link href="/qrcode">QR Code</Link>
+
+      <div className={styles.hero_container}>
+        <div className={styles.overlay}></div>
+        <div className={styles.image}>
+          <img src="/Images/Hero1.jpg" alt="" />
+        </div>
+        {/* <img src="/Images/Hero5.jpg" alt="" /> */}
+        <section className={styles.text_section}>
+          <h1>
+            Creating Brands <br /> That actually gets <br /> attention
+          </h1>
+          <section className={styles.welcome_text}>
+            <hr />
+            <p>
+              Connecting Brands with Engaging Stories. <br /> Welcome to Stories
+              Arabia
+            </p>
+          </section>
+        </section>
+      </div>
     </>
   );
 }
